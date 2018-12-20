49cd05e0f78b46c92744d090944df08f58f6f18e
chenPo <s1310731045@gms.nutc.edu.tw>
2018-12-20 14:44:07 +0800

    括弧防呆優化-自動補後括弧

--- js/calculator.js
+++ js/calculator.js
@@ -3,16 +3,33 @@ String.prototype.LastSearch=function(reg){
     while(i<this.length-1 && this.substring(this.length-i,this.length).search(reg)<0)i++;
     return (this.substring(this.length-i,this.length).search(reg)==0?this.length-i:-1);
 }
+String.prototype.CountSearch=function(reg){
+    var i=0,count=0,value=this;
+    while(value.length>0 && i<value.length){
+        var index=value.substring(i,value.length).search(reg);
+        if(index>-1){
+            count++;
+            value=value.substring(index,value.length);
+        }
+        i++;
+    }
+    return count;
+}
+$('#screen').attr("error","");
 function get_numeral(value,sign){
     return value.substring(value.LastSearch(sign)+1,value.length);
 }
-$('#screen').attr("error","");
 $(".numeral").click(function(){
     var this_value=$(this).html();
     var input_starte=$('#screen').html().trim();
-    var numer=get_numeral(input_starte,/[\+\-\*\/]/);
+    var last_input=input_starte.substring(input_starte.length-1,input_starte.length);
+    var numer=get_numeral(input_starte,/[\(\)\+\-\*\/]/);
+    if($('#screen').attr("error")!=""){
+        $("#screen").html("");
+        $('#screen').attr("error","");
+    }
     if((input_starte=="" && !isNaN(this_value)) || this_value=="("){
-        $("#screen").append(this_value);
+        $("#screen").append((this_value=="(" && ((isFinite(last_input) && input_starte!="") || last_input==")")?"*"+this_value:this_value));
     }else if(this_value=="."){
         if(Number(numer)==0){
             $('#screen').append("0.");
@@ -22,11 +39,8 @@ $(".numeral").click(function(){
     }else if(this_value=="&lt;X"){
         $('#screen').html(input_starte.substring(0,input_starte.length-1));
     }else if(this_value!=""){
-        if(Number(numer)==0 && numer!="" && numer!="0."){
+        if(Number(numer+this_value)==0 && numer!="" && numer!="0."){
             $("#screen").html(input_starte.substring(0,input_starte.length-numer.length));
-        }else if($('#screen').attr("error")!=""){
-            $("#screen").html("");
-            $('#screen').attr("error","");
         }
         $("#screen").append(this_value);
     }
@@ -46,15 +60,20 @@ $(".clear").click(function(){
 });
 $(".ans").click(function(){
     var input=$('#screen').html().trim();
-    input=(input.LastSearch(/[\+\-\*\/]/)==input.length-1?input.substring(0,input.length-1):input);
+    input=(input.LastSearch(/[\+\-\*\//]/)==input.length-1?input.substring(0,input.length-1):input);
+    var befo=input.CountSearch(/[\(]/);
+    while(befo!=input.CountSearch(/[\)]/))input+=")";
+
     try {
+
         var ans=eval(input);
         if(isFinite(ans)){
             $('#screen').html(ans);
         }else{
             $('#screen').html("輸入錯誤");
             $('#screen').attr("error","錯誤");
-        }   
+        }
+
     } catch (error) {
         $('#screen').html("輸入錯誤");
         $('#screen').attr("error","錯誤");
