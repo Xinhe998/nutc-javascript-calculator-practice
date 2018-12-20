bfccf48b8e0d535a9bbb79ab6b5f21021cb6ea59
liunn <genius0970890015@gmail.com>
2018-12-20 10:24:18 +0800

    功能設計

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,34 @@
-document.getElementsByTagName("button").addEventListener('click', function () {
-    alert('this is a box');
+
+var sum="";
+var ans="";
+$("button").click(function(){
+    var obj="";
+    obj=$(this).val();
+    if(obj=="back"){
+        sum=sum.substring(0,sum.length-1)
+    }else{
+        sum+=obj;
+        ans=eval(obj);
+    }
+    /*}else if(obj=="/"){
+        sum+=obj;
+        ans=obj
+    }else if(obj=="*"){
+        sum+=obj;
+        ans=obj;
+    }else if(obj=="-"){
+        sum+=obj;
+        ans=obj;
+    }else if(obj=="+"){
+        sum+=obj;
+        ans=obj;
+    }else if(obj=="clean"){
+        sum+="";
+        ans="";
+    }else if(obj=="equals"){
+        ans=sum;
+        sum=ans;
+    }*/
+    $("#show").val(sum);
+    console.log(ans);
 });
