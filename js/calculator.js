75ac9a218756cfc6e2617bbcd800c21978646865
liunn <genius0970890015@gmail.com>
2018-12-25 17:24:56 +0800

    js加減乘除判斷完成

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,54 @@
 
 var sum="";
-$("button").click(function(){
-    var obj="";
+var obj="";
+var num="";
+var reg=/[\+\-\*\/]/;
+var r="",l="",s="";
+$("button").click(function()
+{
     obj=$(this).val();
-    if(obj=="back"){
-        sum=sum.substring(0,sum.length-1)
-    }else if(obj=="clean"){
-        sum=""
-    }else if(obj=="equals"){
-        sum=eval(sum);
-    }else{
-        sum+=obj;
-    }
+    if(isNaN(obj)==false)
+        number(obj);
+    else
+        symbol(obj);
+
     $("#show").val(sum);
-});
+})
+function number(e)
+{
+    num+=e;
+    sum=num;
+}
+function symbol(e)
+{
+    var show=$("#show").val();
+    if(show.search(reg)>=0)
+    {
+        r=show.substring(0,show.search(reg));
+        l=show.substring(show.search(reg)+1,show.length);
+        s=show.substring(show.search(reg),show.search(reg)+1);
+        cal(r,l,s,e)
+    }else{
+        num+=e;
+    }
+    sum=num;
+}
+function cal(r,l,s,e)
+{
+    console.log(r,s,l,e);
+    switch (s)
+    {
+        case "/" :
+            num = (r / l) + e;
+        break ;
+        case "*" :
+            num = (r * l) + e ;
+        break ;
+        case "-" :
+            num = (r - l) + e ;
+        break ;
+        case "+" :
+            num = (Number(r) + Number(l)) + e ;
+        break ;
+    }
+}
