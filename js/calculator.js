bff54aa53aa7aa293b69af82cf69b39e9fd6b210
ChiaAn <s1310731043@gms.nutc.edu.tw>
2018-12-27 03:02:26 +0800

    修改功能：四則運算、小數、倒退鍵、清除建、總和

--- js/calculator.js
+++ js/calculator.js
@@ -12,63 +12,21 @@ $(document).ready(function () {
 
 
     $(".symbol").click(function () {
-        
-        if (symbolnum == 1) {
-            $("input").val();
-            $("input").val($("input").val() + $(this).val());
-            symbolnum += 1;
-            //alert(symbol);
-        }
-        else {
-            if ($("input").val().indexOf("+") > 0){
-                symbol="+";
-                var split = $("input").val().split("+");
-            }else if($("input").val().indexOf("-") > 0){
-                var split = $("input").val().split("-");
-                symbol="-";
-            }else if($("input").val().indexOf("*") > 0){
-                var split = $("input").val().split("*");
-                symbol="*";
-            }else if($("input").val().indexOf("/") > 0){
-                var split = $("input").val().split("/");
-                symbol="/";
+        var index1 = $("input").val().length;
+        var x = $("input").val().substr((index1 - 1), 1);
+        if (isNaN(x) == true) {
+            alert("錯誤");
+        } else {
+            if (symbolnum == 1) {
+                $("input").val();
+                $("input").val($("input").val() + $(this).val());
+                symbolnum += 1;
+                //alert(symbol);
             }
-            for (var i = 0; i < split.length; i++) {
-                split[i] = parseFloat(split[i]);
+            else {
+                var sum = split();
+                $("input").val(parseFloat(sum) + $(this).val());
             }
-            var sum = split[0];
-            switch (symbol) {
-                case "+":
-
-                    for (var i = 1; i < split.length; i++) {
-                        sum += split[i];
-                    }
-                    //alert(sum);
-                    
-                    break;
-                case "-":
-                    
-                    for (var i = 1; i < split.length; i++) {
-                        sum -= split[i];
-                    }
-                    
-                    break;
-                case "*":
-                    
-                    for (var i = 1; i < split.length; i++) {
-                        sum *= split[i];
-                    }
-                    
-                    break;
-                case "/":
-                    
-                    for (var i = 1; i < split.length; i++) {
-                        sum /= split[i];
-                    }
-                    
-                    break;
-            }
-            $("input").val(parseFloat(sum) + $(this).val());
         }
         dotcc = 0;
     });
@@ -81,61 +39,90 @@ $(document).ready(function () {
     });
 
     $("#with").click(function () {
-        $("input").val();
-        if ($("input").val().indexOf("+") > 0){
-            symbol="+";
-            var split = $("input").val().split("+");
-        }else if($("input").val().indexOf("-") > 0){
-            var split = $("input").val().split("-");
-            symbol="-";
-        }else if($("input").val().indexOf("*") > 0){
-            var split = $("input").val().split("*");
-            symbol="*";
-        }else if($("input").val().indexOf("/") > 0){
-            var split = $("input").val().split("/");
-            symbol="/";
+        var index1 = $("input").val().length;
+        var x = $("input").val().substr((index1 - 1), 1);
+        if (isNaN(x) == true) {
+            alert("錯誤");
         }
-        for (var i = 0; i < split.length; i++) {
-            split[i] = parseFloat(split[i]);
+        else {
+            var sum = split();
+            $("input").val(parseFloat(sum));
+            symbolnum = 1;
+            dotcc = 0;
         }
-        var sum = split[0];
-        switch (symbol) {
-            case "+":
-                for (var i = 1; i < split.length; i++) {
-                    sum += split[i];
-                }
-                //alert(sum);
-                break;
-            case "-":
-                for (var i = 1; i < split.length; i++) {
-                    sum -= split[i];
-                }
-                break;
-            case "*":
-                for (var i = 1; i < split.length; i++) {
-                    sum *= split[i];
-                }
-                break;
-            case "/":
-                for (var i = 1; i < split.length; i++) {
-                    sum /= split[i];
-                }
-                break;
-        }
-        $("input").val(parseFloat(sum));
-        symbolnum = 1;
-        dotcc = 0;
     });
 
     $("#dot").click(function () {
         $("input").val();
-        var index1 =$("input").val().length;
-        var x =$("input").val().substr((index1-1), 1);
-        if (isNaN(x)==false && dotcc == 0){
-            $("input").val($("input").val() + $(this).val());
-            dotcc =1 ;
-        }else {
+        var index1 = $("input").val().length;
+        var x = $("input").val().substr((index1 - 1), 1);
+        if ($("input").val().indexOf(".") > 0){
             alert("錯誤");
+        }else if (isNaN(x) == true && dotcc != 0) {
+            alert("錯誤");
+        } else {
+            $("input").val($("input").val() + $(this).val());
+            dotcc = 1;
         }
     });
+    $("#back").click(function () {
+        var index1 = $("input").val().length;
+        var x = $("input").val().substr((index1 - 1), 1);
+        if (x == "." && dotcc != 0) {
+            dotcc = 0;
+        } else if (isNaN(x) == true && symbolnum != 1) {
+            symbolnum = 1;
+        }
+        var str = $("input").val();
+        $("input").val(str.substr(0, (str.length - 1)));
+    });
+
+    function split() {
+        $("input").val();
+        if (isNaN($("input").val()) == false){
+            sum = $("input").val();
+        }else {
+            if ($("input").val().indexOf("+") > 0) {
+                symbol = "+";
+                var split = $("input").val().split("+");
+            } else if ($("input").val().indexOf("-") > 0) {
+                var split = $("input").val().split("-");
+                symbol = "-";
+            } else if ($("input").val().indexOf("*") > 0) {
+                var split = $("input").val().split("*");
+                symbol = "*";
+            } else if ($("input").val().indexOf("/") > 0) {
+                var split = $("input").val().split("/");
+                symbol = "/";
+            }
+            for (var i = 0; i < split.length; i++) {
+                split[i] = parseFloat(split[i]);
+            }
+            var sum = split[0];
+            switch (symbol) {
+                case "+":
+                    for (var i = 1; i < split.length; i++) {
+                        sum += split[i];
+                    }
+                    //alert(sum);
+                    break;
+                case "-":
+                    for (var i = 1; i < split.length; i++) {
+                        sum -= split[i];
+                    }
+                    break;
+                case "*":
+                    for (var i = 1; i < split.length; i++) {
+                        sum *= split[i];
+                    }
+                    break;
+                case "/":
+                    for (var i = 1; i < split.length; i++) {
+                        sum /= split[i];
+                    }
+                    break;
+            }
+        }
+        return sum;
+    }
 });
